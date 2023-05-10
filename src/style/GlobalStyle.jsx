a43dd82fad6a1327d88bf193a405b62bfe1d3797
Bernardo Marquez <marquez.cmbh@gmail.com>
2023-05-10 11:46:21 -0300

    build: reset styled + styled components

---
+++ src/style/GlobalStyle.jsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+* {
+    font-family: 'Poppins', sans-serif;
+    color: #DCDCE1;
+}
+body {
+    background-color: #121214;
+}
+`
+
+export default GlobalStyle;
