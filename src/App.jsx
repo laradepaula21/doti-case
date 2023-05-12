b73ff1d5b7e56e19e536c9619f58831382ed805e
Bernardo Marquez <marquez.cmbh@gmail.com>
2023-05-12 17:23:00 -0300

    feat: header + pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Blog from './pages/Blog';
+import Products from './pages/Products';
+
 export default function App() {
   return (
-    <div>
-      Primeiro commit
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/blog" element={<Blog/>} />
+        <Route path="/produtos" element={<Products/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
