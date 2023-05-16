18f6b7dfabb692c625284f9b8392a423b8ebec2a
jefferson13t <jefersonpereira1331@gmail.com>
2023-05-16 14:19:37 -0300

    Adicionando estrutura de pastas

--- src/pages/Blog/Post.jsx
+++ src/pages/Blog/Post.jsx
@@ -1,24 +1,28 @@
 import styled from "styled-components"
-import carreira from '../assets/Carreira_1.png'
 
-export default function Post() {
+export default function Post(props) {
+
+    
+    const dados  = props.dados;
 
     return (
         <StyledPost>
+            <img src={dados.imageSource}></img>
             <div>
-                <h1>Carreira</h1>
-                <p>Boas práticas para devs em início de carreira</p>
+                <h1>{dados.titulo}</h1>
+                <p>{dados.corpo}</p>
             </div>
-            <p className="data">Há um ano • 4 min de leitura</p>
+            <p className="data">{dados.horario}</p>
         </StyledPost>
     )
 }
+    
 
 const StyledPost = styled.div`
 
     font-family: 'Poppins', sans-serif;
-    background: url(${carreira});
     width: 30%;
+    min-width: 420px;
     height: 250px;
     color: white;
     overflow: hidden;
@@ -28,7 +32,16 @@ const StyledPost = styled.div`
     border-radius: 20px;
     padding: 8px 15px;
     font-size: 20px;
+    position: relative;
 
+    img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: -1;
+        width: 100%;
+        height: 100%;
+    }
     div{
         height: 50%;
         display: flex;
