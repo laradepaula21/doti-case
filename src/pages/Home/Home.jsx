06aaccb0cbc21ccfc6518620e5626536b505f79b
laraeow <lara00depaula@gmail.com>
2023-05-18 13:30:49 -0300

    iniciando pagina home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,36 @@
 import Header from "../../components/Header";
+import styled from "styled-components";
 
 export default function Home() {
 
     return (
     <>
         <Header underline="home"></Header>
-        aaaaaaaaaaaaa
+        <div>
+            <div>
+                Escolha o nome do membro
+            </div>
+                <StyledLogados>
+                    <div>
+                        <h2> Membros </h2>
+                        <h2> Modalidade </h2>
+                        <h2> Chegada </h2>
+                        <h2> Tempo </h2>
+
+                    </div>
+                     
+                    <div>
+
+                    </div>
+                </StyledLogados>
+        </div>
     </>
     )
 }
+
+const StyledLogados = styled.div`
+    width: 50%;
+
+    background-color: green;
+
+`
