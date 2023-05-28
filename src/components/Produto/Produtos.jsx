a887c30b74f8d23059bf5e6164d5b67f8bbd660c
SamanthaKelly01 <samanthak2002@hotmail.com>
2023-05-28 19:49:36 -0300

    bugs

--- src/components/Produto/Produtos.jsx
+++ src/components/Produto/Produtos.jsx
@@ -14,7 +14,7 @@ export default function Produtos(props) {
                 </div>
                 <StyledContainer>
                     <div>                    
-                        <h1> {item.titulo} </h1>
+                        <h1> {item.titulo} </h1> 
                         <h2> {item.texto} </h2>
                     </div> 
                     <a href="https://www.rocketseat.com.br/" target="_blank"><RightCircleOutlined/></a>
